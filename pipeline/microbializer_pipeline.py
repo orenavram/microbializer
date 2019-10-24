9242cbba2b7db5d7f4fed645f4dd237bfcfb5583
Oren <orenavram@gmail.com>
2019-10-24 13:29:36 +0300

    Adding a more informative message when there are non ascii characters in the fasta file

--- pipeline/microbializer_pipeline.py
+++ pipeline/microbializer_pipeline.py
@@ -37,32 +37,36 @@ def verify_fasta_format(data_path):
             line_number = 1
             line = f.readline()
             if not line.startswith('>'):
-                return f'Illegal <a href="https://www.ncbi.nlm.nih.gov/blast/fasta.shtml" target="_blank">FASTA format</a>.<br>\nFirst line in "{file_name}" starts with "{line[0]}" instead of ">").'
+                return f'Illegal <a href="https://www.ncbi.nlm.nih.gov/blast/fasta.shtml" target="_blank">FASTA format</a>. First line in "{file_name}" starts with "{line[0]}" instead of ">").'
             previous_line_was_header = True
             putative_end_of_file = False
             curated_content = f'>{strain_name}_{line[1:]}'.replace("|", "_")
-            for line in f:
-                line_number += 1
-                line = line.strip()
-                if not line:
-                    if not putative_end_of_file: # ignore trailing empty lines
-                        putative_end_of_file = line_number
-                    continue
-                if putative_end_of_file:  # non empty line after empty line
-                    return f'Illegal <a href="https://www.ncbi.nlm.nih.gov/blast/fasta.shtml" target="_blank">FASTA format</a>.<br>\nLine {putative_end_of_file} in "{file_name}" is empty.'
-                if line.startswith('>'):
-                    if previous_line_was_header:
-                        return f'Illegal <a href="https://www.ncbi.nlm.nih.gov/blast/fasta.shtml" target="_blank">FASTA format</a>.<br>\n"{file_name}" contains an empty record. Both lines {line_number-1} and {line_number} start with ">".'
-                    else:
-                        previous_line_was_header = True
-                        curated_content += f'>{strain_name}_{line[1:]}\n'.replace("|", "_")
+            try:
+                for line in f:
+                    line_number += 1
+                    line = line.strip()
+                    if not line:
+                        if not putative_end_of_file: # ignore trailing empty lines
+                            putative_end_of_file = line_number
                         continue
-                else:  # not a header
-                    previous_line_was_header = False
-                    for c in line:
-                        if c not in legal_chars:
-                            return f'Illegal <a href="https://www.ncbi.nlm.nih.gov/blast/fasta.shtml" target="_blank">FASTA format</a>.<br>\nLine {line_number} in "{file_name}" contains illegal dna character "{c}".'
-                    curated_content += f'{line}\n'
+                    if putative_end_of_file:  # non empty line after empty line
+                        return f'Illegal <a href="https://www.ncbi.nlm.nih.gov/blast/fasta.shtml" target="_blank">FASTA format</a>. Line {putative_end_of_file} in "{file_name}" is empty.'
+                    if line.startswith('>'):
+                        if previous_line_was_header:
+                            return f'Illegal <a href="https://www.ncbi.nlm.nih.gov/blast/fasta.shtml" target="_blank">FASTA format</a>. "{file_name}" contains an empty record. Both lines {line_number-1} and {line_number} start with ">".'
+                        else:
+                            previous_line_was_header = True
+                            curated_content += f'>{strain_name}_{line[1:]}\n'.replace("|", "_")
+                            continue
+                    else:  # not a header
+                        previous_line_was_header = False
+                        for c in line:
+                            if c not in legal_chars:
+                                return f'Illegal <a href="https://www.ncbi.nlm.nih.gov/blast/fasta.shtml" target="_blank">FASTA format</a>. Line {line_number} in "{file_name}" contains illegal dna character "{c}".'
+                        curated_content += f'{line}\n'
+            except UnicodeDecodeError:
+                line_number += 1  # the line that was failed to be read
+                return f'Illegal <a href="https://www.ncbi.nlm.nih.gov/blast/fasta.shtml" target="_blank">FASTA format</a>. Line {line_number} in "{file_name}" contains one (or more) non <a href="https://en.wikipedia.org/wiki/ASCII" target="_blank">ascii</a> character(s).'
         # override the old file with the curated content
         with open(file_path, 'w') as f:
             f.write(curated_content)
