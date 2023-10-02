3ab6709ebe381e531453d04db78c29f7ce55fb62
Yair Shimony <yairshsh@gmail.com>
2023-10-02 13:21:49 +0300

    add ANI

---
+++ pipeline/auxiliaries/steps_auxiliaries.py
@@ -0,0 +1,63 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+
+def aggregate_ani_results(ani_output_dir):
+    all_dfs = []
+    for file_name in os.listdir(ani_output_dir):
+        if not file_name.endswith('.tsv'):
+            continue
+        file_path = os.path.join(ani_output_dir, file_name)
+        df = pd.read_csv(file_path, delimiter='\t',
+                         names=['query', 'subject', 'ani_value', 'orthologous_segments', 'total_segments'])
+        all_dfs.append(df)
+
+    combined_df = pd.concat(all_dfs, ignore_index=True)
+
+    query_short = []
+    subject_short = []
+    for index, row in combined_df.iterrows():
+        query_short.append(os.path.splitext(os.path.basename(row['query']))[0])
+        subject_short.append(os.path.splitext(os.path.basename(row['subject']))[0])
+
+    combined_df['query'] = query_short
+    combined_df['subject'] = subject_short
+
+    ani_values_df = combined_df.pivot_table(index='query', columns='subject', values='ani_value')
+
+    #plt.subplots(figsize=(40, 40))
+    plot = sns.heatmap(ani_values_df, cmap='Blues')
+    fig = plot.get_figure()
+    fig.savefig(os.path.join(ani_output_dir, 'heatmap.png'))
+
+    ani_values_df.replace({100: np.nan}, inplace=True)
+    max_values = ani_values_df.max(axis=1)
+    max_columns = ani_values_df.idxmax(axis=1)
+    ani_values_df = ani_values_df.assign(max_value=max_values.values)
+    ani_values_df = ani_values_df.assign(max_column=max_columns.values)
+    ani_values_df.to_csv(os.path.join(ani_output_dir, 'ani_pairwise_values.csv'))
+
+
+def mimic_prodigal_output(orfs_dir, output_orf_file_extension):
+    for file_name in os.listdir(orfs_dir):
+        file_path = os.path.join(orfs_dir, file_name)
+
+        # edit headers of ORFs to match the structure of prodigal output
+        fixed_content = ''
+        with open(file_path, 'r') as orfs_file:
+            for line in orfs_file:
+                if line.startswith('>'):
+                    fixed_content += f'{line.strip()} # START # END # 1 # \n'
+                else:
+                    fixed_content += line
+
+        # override the old file with the fixed content
+        with open(file_path, 'w') as f:
+            f.write(fixed_content)
+
+        # change file name to match the output of step 2
+        os.rename(file_path, f'{os.path.splitext(file_path)[0]}.{output_orf_file_extension}')
+
