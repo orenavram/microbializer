eac3bfe70cc01f72b3e31c7841ac2061e061f721
Yair Shimony <yairshsh@gmail.com>
2025-01-01 18:33:07 +0200

    wip

--- pipeline/steps/prepare_og_for_mcl.py
+++ pipeline/steps/prepare_og_for_mcl.py
@@ -2,11 +2,11 @@
 from sys import argv
 import argparse
 import logging
-import itertools
 import os
 import sys
 import traceback
 import pandas as pd
+from collections import defaultdict
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
@@ -15,8 +15,8 @@ from auxiliaries.pipeline_auxiliaries import get_job_logger
 from auxiliaries.logic_auxiliaries import flatten
 
 
-def load_all_ogs_hits(all_reciprocal_hits_path, all_relevant_genes):
-    gene_pair_to_score = {}
+def load_all_ogs_hits(all_reciprocal_hits_path, gene_to_og):
+    og_to_gene_pair_to_score = defaultdict(dict)
     with open(all_reciprocal_hits_path) as f:
         for line in f:
             line_tokens = line.rstrip().split(',')
@@ -24,11 +24,13 @@ def load_all_ogs_hits(all_reciprocal_hits_path, all_relevant_genes):
                 # new reciprocal hits file starts
                 continue
             gene_1, gene_2 = line_tokens[:2]
-            if gene_1 in all_relevant_genes:  # gene_1 in all_relevant_genes <==> gene_2 in all_relevant_genes
+            if gene_1 in gene_to_og:  # if gene_1 is in gene_to_og and mapped to OG x, then gene_2 is also in OG x
+                                      # (becuase each putative OG is a conneceted component of the graph described in all_reciprocal_hits_path)
+                og = gene_to_og[gene_1]
                 score = line_tokens[2]
-                gene_pair_to_score[(gene_1, gene_2)] = score
+                og_to_gene_pair_to_score[og][(gene_1, gene_2)] = score
 
-    return gene_pair_to_score
+    return og_to_gene_pair_to_score
 
 
 def prepare_ogs_for_mcl(logger, all_reciprocal_hits_path, putative_ogs_path, job_input_path, output_path):
@@ -37,30 +39,31 @@ def prepare_ogs_for_mcl(logger, all_reciprocal_hits_path, putative_ogs_path, job
         ogs_numbers = [f'OG_{num}' for num in f.read().splitlines()]
 
     logger.info(f'Aggregating all genes from the specified {len(ogs_numbers)} putative OGs...')
-    all_relevant_genes = set()
-    og_to_genes = {}
+    # all_relevant_genes = set()
+    # og_to_genes = {}
+    gene_to_og = {}
     for og_number in ogs_numbers:
         og_row = putative_ogs_df.loc[og_number]
         og_genes = flatten([strain_genes.split(';') for strain_genes in og_row if not pd.isna(strain_genes)])
-        og_to_genes[og_number] = og_genes
-        all_relevant_genes.update(og_genes)
-    logger.info(f'All relevant genes were aggregated successfully. Number of relevant genes is {len(all_relevant_genes)}.')
+        # og_to_genes[og_number] = og_genes
+        # all_relevant_genes.update(og_genes)
+        for gene in og_genes:
+            gene_to_og[gene] = og_number
+    logger.info(f'All relevant genes were aggregated successfully. Number of relevant genes is {len(gene_to_og)}.')
 
     logger.info('Loading relevant hits scores to memory...')
-    gene_pair_to_score = load_all_ogs_hits(all_reciprocal_hits_path, all_relevant_genes)
-    logger.info(f'All relevant hits were loaded successfully. Number of relevant hits (gene pairs) is {len(gene_pair_to_score)}.')
+    og_to_gene_pair_to_score = load_all_ogs_hits(all_reciprocal_hits_path, gene_to_og)
+    logger.info(f'All relevant hits were loaded successfully.')
 
     logger.info('Preparing input files for MCL...')
-    for og_number, genes in og_to_genes.items():
+    for og_number in ogs_numbers:
         mcl_file_path = os.path.join(output_path, og_number + '.mcl_input')
         if os.path.exists(mcl_file_path):
             continue
 
         og_text_for_mcl = ''
-        for gene1, gene2 in itertools.combinations(genes, 2):
-            score = gene_pair_to_score.get((gene1, gene2), gene_pair_to_score.get((gene2, gene1)))
-            if score:
-                og_text_for_mcl += f'{gene1}\t{gene2}\t{score}\n'
+        for (gene1, gene2), score in og_to_gene_pair_to_score[og_number].items():
+            og_text_for_mcl += f'{gene1}\t{gene2}\t{score}\n'
 
         with open(mcl_file_path, 'w') as mcl_f:
             mcl_f.write(og_text_for_mcl)
