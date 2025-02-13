852c756fd282c4591ae1ac0d67b6b503837f21f4
Yair Shimony <yairshsh@gmail.com>
2025-02-13 17:51:03 +0200

    add rf results

--- tests/panx_optimizations/rf_distance.py
+++ tests/panx_optimizations/rf_distance.py
@@ -8,6 +8,7 @@ DATA_DIR = SCRIPT_DIR / 'salmonella_300_c_001'
 
 ORIGINAL_ORTHOGROUPS_INFERENCE_ALL_CORE_OGS = DATA_DIR / 'original' / 'tree_all_core_ogs' / 'final_species_tree.newick'
 ORIGINAL_ORTHOGROUPS_INFERENCE_1000_CORE_OGS = DATA_DIR / 'original' / 'tree_1000_core_ogs' / 'final_species_tree.newick'
+OPTIMIZED_ORTHOGROUPS_INFERENCE_ALL_CORE_OGS = DATA_DIR / 'pseudo_batchsize_mincomparisons' / 'tree_all_core_ogs' / 'final_species_tree.newick'
 OPTIMIZED_ORTHOGROUPS_INFERENCE_1000_CORE_OGS = DATA_DIR / 'pseudo_batchsize_mincomparisons' / 'tree_1000_core_ogs' / 'final_species_tree.newick'
 
 
@@ -32,6 +33,9 @@ def main():
     with open(ORIGINAL_ORTHOGROUPS_INFERENCE_1000_CORE_OGS, "r") as f:
         original_orthogorups_inference_1000_core_ogs_tree = Tree(f.readline().strip())
 
+    with open(OPTIMIZED_ORTHOGROUPS_INFERENCE_ALL_CORE_OGS, "r") as f:
+        optimized_orthogorups_inference_all_core_ogs_tree = Tree(f.readline().strip())
+
     with open(OPTIMIZED_ORTHOGROUPS_INFERENCE_1000_CORE_OGS, "r") as f:
         optimized_orthogorups_inference_1000_core_ogs_tree = Tree(f.readline().strip())
 
@@ -39,15 +43,19 @@ def main():
                                original_orthogorups_inference_1000_core_ogs_tree,
                                'original_all_core_ogs', 'original_1000_core_ogs')
 
-    result2_df = compare_trees(original_orthogorups_inference_all_core_ogs_tree,
+    result2_df = compare_trees(optimized_orthogorups_inference_all_core_ogs_tree,
+                               optimized_orthogorups_inference_1000_core_ogs_tree,
+                               'optimized_all_core_ogs', 'optimized_1000_core_ogs')
+
+    result3_df = compare_trees(original_orthogorups_inference_all_core_ogs_tree,
+                               optimized_orthogorups_inference_all_core_ogs_tree,
+                               'original_all_core_ogs', 'optimized_all_core_ogs')
+
+    result4_df = compare_trees(original_orthogorups_inference_all_core_ogs_tree,
                                optimized_orthogorups_inference_1000_core_ogs_tree,
                                'original_all_core_ogs', 'optimized_1000_core_ogs')
 
-    result3_df = compare_trees(original_orthogorups_inference_1000_core_ogs_tree,
-                               optimized_orthogorups_inference_1000_core_ogs_tree,
-                               'original_1000_core_ogs', 'optimized_1000_core_ogs')
-
-    results_df = pd.concat([result1_df, result2_df, result3_df], ignore_index=True)
+    results_df = pd.concat([result1_df, result2_df, result3_df, result4_df], ignore_index=True)
     results_df.to_csv(DATA_DIR / 'trees_comparison_results.csv', index=False)
 
 
